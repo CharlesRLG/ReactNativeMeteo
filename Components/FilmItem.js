b017c9543eefaf45b8b158b321d99fdbdd2bfc3e
charles <charlesrabeclegloahec@gmail.com>
2022-03-29 10:23:25 +0200

    mises en place API

---
+++ Components/FilmItem.js
@@ -0,0 +1,77 @@
+// Components/FilmItem.js
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import dayjs from 'dayjs'
+// import 'react-json-pretty/themes/adventure_time.css'
+// import JSONPretty from 'react-json-pretty'
+// import { getImageFromApi } from '../API/TMDBApi'
+
+class FilmItem extends React.Component {
+  render() {
+    const film = this.props.film
+    console.log(this.props.film.title)
+    return (
+      <View>
+        <View>
+          {/* <JSONPretty data={film}></JSONPretty> */}
+        </View>
+        <View style={styles.film_main_container}>
+        {/* <Image style={styles.image} source={getImageFromApi(film.poster_path)} /> */}
+
+          <View style={styles.film_description}>
+            <View style={styles.titreVote}>
+              <Text style={styles.titreFilm}>{film.title}</Text>
+              <Text style={styles.Vote_average_film}>{film.vote_average}</Text>
+            </View>
+            <Text style={styles.OverviewFilm}>{film.overview}</Text>
+            <Text style={styles.release_date_film}>
+              {dayjs(film.release_date).format('DD/MM/YYYY')}
+            </Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  film_main_container: {
+    flexDirection: 'row',
+    margin: 20,
+    backgroundColor: '#FFFFF',
+  },
+  film_description: {
+    flexDirection:'column',
+    marginLeft: 20,
+  }, 
+  titreVote: {
+    flexDirection: 'row',
+    fontSize: 16,
+  },
+  titreFilm: {
+    marginRight: 50,
+    fontSize: 20,
+    minWidth: 300,
+    fontWeight: 'bold',
+  },
+  Vote_average_film: {
+    fontWeight: 'bold',
+  },
+  OverviewFilm: {
+    marginTop: 20,
+    marginBottom: 20,
+    maxWidth: 350,
+    minHeight: 100,
+  },
+  release_date_film: {
+    fontSize: 16,
+    marginLeft: 250,
+    fontWeight: 'bold',
+  },
+  image: {
+        width: 200,
+        height: 300,
+        flexDirection:'column',
+    }
+    
+})
+export default FilmItem
--- Components/Search.js
+++ Components/Search.js
@@ -1,19 +1,91 @@
 // Components/Search.js
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { FlatList, View, TextInput, Button, StyleSheet } from 'react-native'
+// import films from '../Helpers/filmsData'
+import FilmItem from '../Components/FilmItem'
+// import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+import { ActivityIndicator } from 'react-native'
 
+const styles = StyleSheet.create({
+  main_container: {
+    marginTop: 20,
+    flex: 1,
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5,
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 class Search extends React.Component {
-    
-            render() {
+  render() {
+    console.log('RENDER')
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          placeholder='Choisissez une ville:'
+          style={{ styles }}
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title="Rechercher" onPress={() => this._loadFilms()} />
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <FilmItem film={item} />}
+        />
+      { this._displayLoading() }
+      </View>
 
-                return (
-                    <View>
-                        <TextInput placeholder='Choisir une ville' />
-                        <Button title='Afficher la météo' onPress={() => { }} />
-                    </View>
-            )
-        }
+    )
+  }
+  constructor(props) {
+    super(props)
+    this.searchedText = ''
+    this.state = {
+      films: [],
+      isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
     }
+    
+  }
 
+  // Nouvelle méthode
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par setState
+  }
+
+ // Bien noter les deux setState
+ //   isLoading: True puis appel API puis lorsque l'API a répondu isLoading: False
+ _loadFilms() {
+    if (this.state.isLoading) return
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText).then((data) => {
+        this.setState({ films: data.results, isLoading: false });
+      });
+    }
+  }
+    _displayLoading() {
+      if (this.state.isLoading) {
+        return (
+          <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
+}
 export default Search
